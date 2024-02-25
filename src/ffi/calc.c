986c5c871cb5ca5347216e173fba2ef99e41a52e
yasuaki640 <yasuaki640@gmail.com>
2024-02-25 20:30:04 +0900

    delete gomi

--- src/ffi/calc.c
+++
@@ -1,8 +0,0 @@
-int factorial(int n) {
-    int fact = 1;
-    for (int i = 1; i <= n; ++i) {
-        fact = fact * i;
-    };
-
-    return fact;
-}
--- src/ffi/echo.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void echo_ffi() {
-    printf("Hello from FFI!!!");
-}
